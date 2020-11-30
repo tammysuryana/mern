53d143a31f1298ee99969960da0ed2e0b21532f4
TAMMY SURYANA <tammysuryana@gmail.com>
2020-12-01 01:59:22 +0700

    stuktur folder dan tambahan react router dom

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+//import React from 'raect';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jadidewa.com"
           target="_blank"
           rel="noopener noreferrer"
         >
